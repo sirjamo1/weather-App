0a6303e4ee8f4b000793b0d3efbaf8e78806541e
Sirjamo1 <jam_how@hotmail.com>
2022-12-03 06:32:55 +1000

    added background change function (depending on sunrise/sunset to local time)

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # weather-App
+
+
+
+
+backgrounds from www.pexels.com
+
+morning by Travis Rupert 
+midday by Skitterphoto
+sunset by Александр Прокофьев
+night by  brenoanp
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const renderApp = () => {
     const mainContainer = document.createElement("div");
     mainContainer.id = "main-container";
+    mainContainer.style.backgroundImage =
+        "url('assets/images/backgrounds/midday.jpg')";
     mainContainer.appendChild(renderForm());
     // mainContainer.appendChild(renderWeatherCard());
     return mainContainer;
@@ -31,15 +33,45 @@ const renderForm = () => {
     return formContainer;
 };
 // https://api.openweathermap.org/data/2.5/weather?q=brisbane&appid=9f22446b3c7684227930790425851744
-
-// const sunrise = weatherDataResponse.sys.sunrise;
-// const sunset = weatherDataResponse.sys.sunset;
 // const windSpeed = weatherDataResponse.wind.speed;
 // const winDirection = weatherDataResponse.wind.deg;
 
+const changeBackground = (data, sunriseTime, sunsetTime, el) => {
+    const utcSeconds = parseInt(data.dt, 10) + parseInt(data.timezone, 10);
+    const utcMilliseconds = utcSeconds * 1000;
+    const localHour = new Date(utcMilliseconds).getUTCHours();
+    let sunriseHour = Number(sunriseTime.substring(0, 2));
+    let sunsetHour = Number(sunsetTime.substring(0, 2));
+    if (localHour === sunriseHour || localHour === sunriseHour - 1) {
+        el.style.backgroundImage =
+            "url('assets/images/backgrounds/sunrise.jpg')";
+    } else if (localHour === sunsetHour || localHour === sunsetHour + 1) {
+        el.style.backgroundImage =
+            "url('assets/images/backgrounds/sunset.jpg')";
+    } else if (localHour > sunriseHour + 1 && localHour < sunsetHour - 1) {
+        el.style.backgroundImage =
+            "url('assets/images/backgrounds/midday.jpg')";
+    } else {
+        el.style.backgroundImage = "url('assets/images/backgrounds/night.jpg')";
+    }
 
+    console.log(localHour);
+    console.log(sunriseTime);
+    console.log((sunriseHour += 1));
+    console.log(sunsetHour);
+};
+
+///NEED TO match the above with the local sunrise/ sunset else midday/ night
 const renderWeatherCard = (data) => {
+    const sunriseTime = new Date(data.sys.sunrise * 1000)
+        .toTimeString()
+        .substring(0, 8);
+    const sunsetTime = new Date(data.sys.sunset * 1000)
+        .toTimeString()
+        .substring(0, 8);
+
     const mainContainer = document.getElementById("main-container");
+    changeBackground(data, sunriseTime, sunsetTime, mainContainer);
     const weatherCard = document.createElement("div");
     weatherCard.id = "weather-card";
     const location = document.createElement("h3");
@@ -66,6 +98,24 @@ const renderWeatherCard = (data) => {
     feelsLike.id = "feels-like";
     tempContainer.appendChild(feelsLike);
     weatherCard.appendChild(tempContainer);
+    const sunContainer = document.createElement("div");
+    sunContainer.id = "sun-container";
+    const sunrise = document.createElement("p");
+    sunrise.innerHTML = `Sunrise : ${sunriseTime} am`;
+    sunContainer.appendChild(sunrise);
+    const sunset = document.createElement("p");
+    sunset.innerHTML = `Sunset : ${sunsetTime} pm`;
+    sunContainer.appendChild(sunset);
+    weatherCard.appendChild(sunContainer);
+    const windContainer = document.createElement("div");
+    windContainer.id = "wind-container";
+    const windSpeed = document.createElement("p");
+    windSpeed.innerHTML = `Wind speed : ${data.wind.speed}`;
+    windContainer.appendChild(windSpeed);
+    const windDirection = document.createElement("p");
+    windDirection.innerHTML = `Wind direction : ${data.wind.deg}`;
+    windContainer.appendChild(windDirection);
+    weatherCard.appendChild(windContainer);
     if (document.getElementById("weather-card")) {
         mainContainer.replaceChild(
             weatherCard,
@@ -75,7 +125,6 @@ const renderWeatherCard = (data) => {
         mainContainer.appendChild(weatherCard);
     }
 };
-
 const getData = async () => {
     let location = document.getElementById("location").value;
     try {
@@ -84,6 +133,9 @@ const getData = async () => {
             { mode: "cors" }
         );
         const weatherData = await response.json();
+        const lat = weatherData.coord.lat;
+        const lon = weatherData.coord.lon;
+        console.log(weatherData);
         renderWeatherCard(weatherData);
     } catch (err) {
         console.log(err);
@@ -122,7 +174,10 @@ const fahrenheitToCelsius = (f) => {
     let c = Math.round((f - 32) * 0.5556) + " c";
     return c;
 };
-
+// const timeAdjustment = (num) => {
+// const time = new Date(num.dt * 1000 - .timezone * 1000);
+// return time
+// }
 document.querySelector("body").appendChild(renderApp());
 //{temp: 295.86, feels_like: 295.65, temp_min: 295.86, temp_max: 295.86, pressure: 1010, …}
 

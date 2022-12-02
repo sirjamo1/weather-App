b649e4f21958352ed0122fe8443d7df65cdb1e41
Sirjamo1 <jam_how@hotmail.com>
2022-12-02 10:47:56 +1000

    weather card intial setup complete

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const renderApp = () => {
     const mainContainer = document.createElement("div");
     mainContainer.id = "main-container";
     mainContainer.appendChild(renderForm());
+    mainContainer.appendChild(renderWeatherCard());
     return mainContainer;
 };
 
@@ -17,22 +18,57 @@ const renderForm = () => {
     locationInput.name = "location";
     locationInput.id = "location";
     formContainer.appendChild(locationInput);
-    const submitButton = document.createElement("button");
-    submitButton.type = "submit";
-    submitButton.innerHTML = "Search";
-    formContainer.appendChild(submitButton);
+    const searchButton = document.createElement("button");
+    searchButton.type = "search";
+    searchButton.innerHTML = "Search";
+    searchButton.addEventListener('click', (e) => {
+     getData();
+    e.preventDefault();
+    })
+
+    formContainer.appendChild(searchButton);
 
     return formContainer;
 };
-
+// https://api.openweathermap.org/data/2.5/weather?q=brisbane&appid=9f22446b3c7684227930790425851744
 const renderWeatherCard = () => {
     const weatherCard = document.createElement("div");
     weatherCard.id = "weather-card";
     const location = document.createElement("h3");
-    location.innerHTML = "London";
+    location.innerHTML = `London`;
     weatherCard.appendChild(location);
+    const tempContainer = document.createElement("div");
+    tempContainer.id = "temp-container";
+    const temp = document.createElement("p");
+    temp.innerHTML = `Temp : `;
+    temp.id = "temp";
+    tempContainer.appendChild(temp);
+    const minTemp = document.createElement("p");
+    minTemp.innerHTML = `Min-temp : `;
+    minTemp.id = "min-temp";
+    tempContainer.appendChild(minTemp);
+    const maxTemp = document.createElement("p");
+    maxTemp.innerHTML = `Max-temp : `;
+    maxTemp.id = "max-temp";
+    tempContainer.appendChild(maxTemp);
+    const feelsLike = document.createElement("p");
+    feelsLike.innerHTML = `Feels like : `;
+    feelsLike.id = "feels-like";
+    tempContainer.appendChild(feelsLike);
+    weatherCard.appendChild(tempContainer);
 
     return weatherCard;
 };
 
+const getData = async () => {
+ let location = document.getElementById('location').value;
+  try {
+   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=9f22446b3c7684227930790425851744`, {mode: 'cors'});
+   const weatherData = await response.json();
+   console.log(weatherData)
+  } catch(err) {
+   console.log(err)
+  }
+
+}
 document.querySelector("body").appendChild(renderApp());

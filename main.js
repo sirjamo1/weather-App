30074c5f3f43bbc889b345d0dc989d288bcfdb65
Sirjamo1 <jam_how@hotmail.com>
2022-12-02 11:50:41 +1000

    api working correctly

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const renderApp = () => {
     const mainContainer = document.createElement("div");
     mainContainer.id = "main-container";
     mainContainer.appendChild(renderForm());
-    mainContainer.appendChild(renderWeatherCard());
+    // mainContainer.appendChild(renderWeatherCard());
     return mainContainer;
 };
 
@@ -21,54 +21,93 @@ const renderForm = () => {
     const searchButton = document.createElement("button");
     searchButton.type = "search";
     searchButton.innerHTML = "Search";
-    searchButton.addEventListener('click', (e) => {
-     getData();
-    e.preventDefault();
-    })
+    searchButton.addEventListener("click", (e) => {
+        getData();
+        e.preventDefault();
+    });
 
     formContainer.appendChild(searchButton);
 
     return formContainer;
 };
 // https://api.openweathermap.org/data/2.5/weather?q=brisbane&appid=9f22446b3c7684227930790425851744
-const renderWeatherCard = () => {
+const renderWeatherCard = (
+    locationData,
+    tempData,
+    minTempData,
+    maxTempData,
+    feelsLikeData
+) => {
+    const mainContainer = document.getElementById("main-container");
     const weatherCard = document.createElement("div");
     weatherCard.id = "weather-card";
     const location = document.createElement("h3");
-    location.innerHTML = `London`;
+    location.innerHTML =
+        locationData[0].toUpperCase() + locationData.slice(1).toLowerCase();
     weatherCard.appendChild(location);
     const tempContainer = document.createElement("div");
     tempContainer.id = "temp-container";
     const temp = document.createElement("p");
-    temp.innerHTML = `Temp : `;
+    temp.innerHTML = `Temp : ${tempData}`;
     temp.id = "temp";
     tempContainer.appendChild(temp);
     const minTemp = document.createElement("p");
-    minTemp.innerHTML = `Min-temp : `;
+    minTemp.innerHTML = `Min-temp : ${minTempData}`;
     minTemp.id = "min-temp";
     tempContainer.appendChild(minTemp);
     const maxTemp = document.createElement("p");
-    maxTemp.innerHTML = `Max-temp : `;
+    maxTemp.innerHTML = `Max-temp : ${maxTempData}`;
     maxTemp.id = "max-temp";
     tempContainer.appendChild(maxTemp);
     const feelsLike = document.createElement("p");
-    feelsLike.innerHTML = `Feels like : `;
+    feelsLike.innerHTML = `Feels like : ${feelsLikeData}`;
     feelsLike.id = "feels-like";
     tempContainer.appendChild(feelsLike);
     weatherCard.appendChild(tempContainer);
-
-    return weatherCard;
+    if (document.getElementById("weather-card")) {
+        mainContainer.replaceChild(
+            weatherCard,
+            document.getElementById("weather-card")
+        );
+    } else {
+        mainContainer.appendChild(weatherCard);
+    }
 };
 
 const getData = async () => {
- let location = document.getElementById('location').value;
-  try {
-   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=9f22446b3c7684227930790425851744`, {mode: 'cors'});
-   const weatherData = await response.json();
-   console.log(weatherData)
-  } catch(err) {
-   console.log(err)
-  }
+    let location = document.getElementById("location").value;
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=9f22446b3c7684227930790425851744`,
+            { mode: "cors" }
+        );
+        const weatherData = await response.json();
+        const tempC = kelvinToCelsius(weatherData.main.temp);
+        const minTempC = kelvinToCelsius(weatherData.main.temp_min);
+        const maxTempC = kelvinToCelsius(weatherData.main.temp_max);
+        const feelsLikeTempC = kelvinToCelsius(weatherData.main.feels_like);
 
-}
+        renderWeatherCard(location, tempC, minTempC, maxTempC, feelsLikeTempC);
+    } catch (err) {
+        console.log(err);
+    }
+};
+const kelvinToCelsius = (k) => {
+    let c = Math.round(k - 273.15) + " c";
+    return c;
+};
+const kelvinToFahrenheit = (k) => {
+    let f = Math.round(1.8 * (k - 273) + 32) + " f";
+    return f;
+};
+const celsiusTofahrenheit = (c) => {
+    let f = Math.round(c * 1.8 + 32) + " f";
+    return f;
+};
+const fahrenheitToCelsius = (f) => {
+    let c = Math.round((f - 32) * 0.5556) + " c";
+    return c;
+};
+
 document.querySelector("body").appendChild(renderApp());
+//{temp: 295.86, feels_like: 295.65, temp_min: 295.86, temp_max: 295.86, pressure: 1010, …}
